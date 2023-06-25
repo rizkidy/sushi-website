53c99ca1c7d3c7a9a2ce372ef6dc444345e7cb96
rizkidy <rizkidy99@gmail.com>
2023-06-25 17:19:57 +0700

    initial commit

---
+++ assets/data/data-sushi.js
@@ -0,0 +1,20 @@
+export const sushi = [
+    {
+        id: 1,
+        name: "Onigiri",
+        price: "$10.99",
+        picture: "./img/popular-onigiri.png"
+    },
+    {
+        id: 2,
+        name: "Spring Rolls",
+        price: "$15.99",
+        picture: "./img/popular-spring-rols.png"
+    },
+    {
+        id: 3,
+        name: "Sushi Rolls",
+        price: "$19.99",
+        picture: "./img/popular-sushi-rolls.png"
+    },
+]
